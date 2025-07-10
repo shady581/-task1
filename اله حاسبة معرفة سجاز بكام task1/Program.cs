33659c7f461f8069ffcc6dc33223b0804b703be0
user <user@DESKTOP-840HSM1>
2025-07-10 14:47:06 +0300

    Add project files.

---
+++ اله حاسبة معرفة سجاز بكام task1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace اله_حاسبة_معرفة_سجاز_بكام_task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Pricesmallroom = 25;
+
+            int Pricesbigroom = 35;
+            double taxrate = 0.06;
+
+            Console.WriteLine("enter number small carpets");
+            int numbersmallcarpets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter number big carpets");
+            int numberbigcarpets = Convert.ToInt32(Console.ReadLine());
+
+            double cost = (numbersmallcarpets * Pricesmallroom) + (numberbigcarpets * Pricesbigroom);
+            Console.WriteLine($" cost is befort taxrate is{cost}");
+
+            double totalcost = (cost * taxrate);
+
+            Console.WriteLine($"total cost after taxrate is{totalcost}");
+
+
+        }
+    }
+}
